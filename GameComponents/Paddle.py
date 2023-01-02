ad91ec163127e8eda3fe3dfc1965823523e01da5
DeathPacito6 <89965221+DeathPacito6@users.noreply.github.com>
2023-01-02 14:57:39 +1000

    create a paddle class and display it on the screen

---
+++ GameComponents/Paddle.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+import pygame
+
+class Paddle:
+    def __init__(self, surface: pygame.Surface, upKey: str = None, downKey: str = None, leftSide: bool = True):
+        self.surface = surface
+        self.width = 15
+        self.height = 80
+        self.top = self.surface.get_height() / 2 - self.height / 2
+        self.wallIndent = 30
+        self.left = self.wallIndent if leftSide else self.surface.get_width() - self.wallIndent - self.width
+
+        self.upKey = upKey
+        self.downKey = downKey
+        self.moveSpeed = 8
+        self.direction = 0 # 1 = up, 0 = none, -1 = down
+        self.maxY = self.surface.get_height()
+        self.minY = 0
+
+        self.colour = (255, 255, 255)
+
+        self.paddle = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        pygame.draw.rect(self.paddle, self.colour, pygame.Rect(0, 0, self.width, self.height))
+
+    def up(self):
+        self.top -= self.moveSpeed
+        if self.top < self.minY:
+            self.top = self.minY
+    
+    def down(self):
+        self.top += self.moveSpeed
+        if self.top + self.height> self.maxY:
+            self.top = self.maxY - self.height
+
+    def render(self):
+        self.surface.blit(self.paddle, (self.left, self.top))
+
+    def update(self, events: list[pygame.event.Event]):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == self.upKey:
+                    self.direction = 1
+                elif event.key == self.downKey:
+                    self.direction = -1
+            if event.type == pygame.KEYUP:
+                if event.key == self.upKey and self.direction == 1:
+                    self.direction = 0
+                if event.key == self.downKey and self.direction == -1:
+                    self.direction = 0
+
+        if self.direction == 1:
+            self.up()
+        elif self.direction == -1:
+            self.down()
--- game.py
+++ game.py
@@ -1,1 +1,38 @@
+import os
+import sys
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
 import pygame
+from GameComponents.Paddle import Paddle
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((650, 480))
+icon = pygame.image.load(os.path.join("./", "icon.png"))
+
+pygame.display.set_caption("Pong!")
+pygame.display.set_icon(icon)
+
+
+opponent = Paddle(screen, pygame.K_w, pygame.K_s)
+player = Paddle(screen, pygame.K_UP, pygame.K_DOWN, False)
+
+# game event loop
+while True:
+    clock.tick(60)
+    print(f'\rFPS: {clock.get_fps():.0f}{" "*8}', end='')
+    screen.fill((0, 0, 0))
+    events = pygame.event.get()
+
+    cursor = pygame.mouse.get_pos()
+    opponent.update(events)
+    player.update(events)
+    opponent.render()
+    player.render()
+
+    for event in events:
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    pygame.display.update()
