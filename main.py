e518867954cc5bb19de4b99169088edff6d728f1
DeathPacito6 <89965221+DeathPacito6@users.noreply.github.com>
2023-01-03 14:33:41 +1000

    fixed the FPS stability problem, game now runs with vsync

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import os
 import sys
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-
 import pygame
 from GameComponents.Game import Game
 
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((650, 480), vsync=1)
+screen = pygame.display.set_mode((650, 480), vsync=True, flags= pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.SCALED )
 icon = pygame.image.load(os.path.join("./", "icon.png"))
 
 pygame.display.set_caption("Pong!")
 pygame.display.set_icon(icon)
 
 game = Game(screen)
-
+FPS = 60
 # game event loop
+
 while True:
-    clock.tick(60)
-    # print(f'\rFPS: {clock.get_fps():.0f}{" "*8}', end='')
+    dt = clock.tick(FPS)
+    print(f'\rFPS: {clock.get_fps():.0f} dt: {dt} hSpeed: {game.puck.speedX:.2f} vSpeed: {game.puck.speedY:.2f} Score: {game.leftScore} | {game.rightScore}', end=' '*10)
     events = pygame.event.get()
     
     screen.fill((0, 0, 0))
